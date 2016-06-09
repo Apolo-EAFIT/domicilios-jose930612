66eb04b2b4d383ce8e62e94bd6932f62605dff8a
Jose Mejia <josemejia@joses-macbook-air.local>
2016-06-09 13:49:29 -0500

    entrega final

--- domicilios/main.cpp
+++ domicilios/main.cpp
@@ -82,133 +82,70 @@ int main(){
         
     }
     domicilios.close();
-    /*
-    for (int i=0; i<domiciliarios; ++i) {
-        cout<<"Domiciliario No. "<<i+1<<endl;
-        for (int j=0; j<=sizeof(ruta[i])/sizeof(int); ++j) {
-            cout<<ruta[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
-    */
     
     for (int i=0; i<domiciliarios; ++i) {
         inicio[i] = ruta[i][0];
     }
     
-    
-    vector<int> min_distance;
-    vector<int> previous;
-    /*
     for (int j=0; j<domiciliarios; ++j) {
-        <#statements#>
-    }
-    */
-    do {
-        if (ruta[0][0] == inicio[0]) {
-            int weightRuta = 0;
-            //cout << ruta[0][0] << "\t" << ruta[0][1] << "\t" << ruta[0][2] << '\n';
-            
-            for (int i=1; i<=sizeof(ruta[0])/sizeof(int); ++i) {
-                DijkstraComputePaths(ruta[0][i-1], adjacency_list, min_distance, previous);
-                weightRuta += min_distance[ruta[0][i]];
+        vector<int> min_distance;
+        vector<int> previous;
+        cout<<"Recorrido domiciliario No. "<<j+1<<":\t";
+        do {
+            if (ruta[j][0] == inicio[j]) {
+                int weightRuta = 0;
                 
-                //el 4 que se usa en min_distance[4] es el destino
-                //cout << "-Distance from "<<ruta[0][i-1]<<" to "<<ruta[0][i]<<": " << min_distance[ruta[0][i]] << endl;
-                /*
-                //aqui tambien el 4 es el destino
-                list<int> path = DijkstraGetShortestPathTo(ruta[0][i], previous);
-                cout << "Path : ";
-                copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
-                cout << endl;
-                */
-            }
-            for (int i=(sizeof(ruta[0])/sizeof(int))-1; i>=0; --i) {
-                DijkstraComputePaths(ruta[0][i+1], adjacency_list, min_distance, previous);
-                weightRuta += min_distance[ruta[0][i]];
-                
-                //el 4 que se usa en min_distance[4] es el destino
-                //cout << "-Distance from "<<ruta[0][i+1]<<" to "<<ruta[0][i]<<": " << min_distance[ruta[0][i]] << endl;
-                /*
-                //aqui tambien el 4 es el destino
-                list<int> path = DijkstraGetShortestPathTo(ruta[0][i], previous);
-                cout << "Path : ";
-                copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
-                cout << endl;
-                 */
-            }
-            //cout<<"Distancia recorrida total = "<<weightRuta<<endl<<endl;
-            if (weightRuta <= rutaMenor) {
-                rutaMenor = weightRuta;
-                for (int i=0; i<=sizeof(ruta[0])/sizeof(int); ++i) {
-                    rutaOptima[0][i] = ruta[0][i];
+                for (int i=1; i<=sizeof(ruta[j])/sizeof(int); ++i) {
+                    DijkstraComputePaths(ruta[j][i-1], adjacency_list, min_distance, previous);
+                    weightRuta += min_distance[ruta[0][i]];
                 }
+                for (int i=(sizeof(ruta[j])/sizeof(int))-1; i>=0; --i) {
+                    DijkstraComputePaths(ruta[j][i+1], adjacency_list, min_distance, previous);
+                    weightRuta += min_distance[ruta[j][i]];
+                }
+                
+                if (weightRuta <= rutaMenor) {
+                    rutaMenor = weightRuta;
+                    for (int i=0; i<=sizeof(ruta[j])/sizeof(int); ++i) {
+                        rutaOptima[j][i] = ruta[j][i];
+                    }
+                }
+                
             }
+        } while ( next_permutation(ruta[j],ruta[j]+3) );
+        
+        for (int i=0; i<=sizeof(rutaOptima[j])/sizeof(int); ++i) {
+            cout<<rutaOptima[j][i]<<"\t";
+        }
+        cout<<endl<<"Distancia recorrida total = "<<rutaMenor<<endl<<endl;
+        for (int i=1; i<=sizeof(rutaOptima[j])/sizeof(int); ++i) {
+            
+            DijkstraComputePaths(rutaOptima[j][i-1], adjacency_list, min_distance, previous);
+            
+            //el 4 que se usa en min_distance[4] es el destino
+            cout << "from "<<rutaOptima[j][i-1]<<" to "<<rutaOptima[j][i]<<": ";
+            
+            //aqui tambien el 4 es el destino
+            list<int> path = DijkstraGetShortestPathTo(rutaOptima[j][i], previous);
+            copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
+            cout << endl;
             
         }
-    } while ( next_permutation(ruta[0],ruta[0]+3) );
-    
-    
-    
-    cout<<"Distancia recorrida total = "<<rutaMenor<<endl<<endl;
-    for (int i=1; i<=sizeof(rutaOptima[0])/sizeof(int); ++i) {
-        //cout<<rutaOptima[0][i]<<"\t";
-        DijkstraComputePaths(rutaOptima[0][i-1], adjacency_list, min_distance, previous);
-        
-        //el 4 que se usa en min_distance[4] es el destino
-        cout << "from "<<rutaOptima[0][i-1]<<" to "<<rutaOptima[0][i]<<": " /*min_distance[rutaOptima[0][i]] << */;
-        
-         //aqui tambien el 4 es el destino
-         list<int> path = DijkstraGetShortestPathTo(rutaOptima[0][i], previous);
-         copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
-         cout << endl;
-        
+        for (int i=(sizeof(ruta[j])/sizeof(int))-1; i>=0; --i) {
+            DijkstraComputePaths(rutaOptima[j][i+1], adjacency_list, min_distance, previous);
+            
+            //el 4 que se usa en min_distance[4] es el destino
+            cout << "from "<<rutaOptima[j][i+1]<<" to "<<rutaOptima[j][i]<<": ";
+            
+            //aqui tambien el 4 es el destino
+            list<int> path = DijkstraGetShortestPathTo(rutaOptima[j][i], previous);
+            copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
+            cout << endl;
+            
+        }
+        cout<<endl;
     }
-    for (int i=(sizeof(ruta[0])/sizeof(int))-1; i>=0; --i) {
-        DijkstraComputePaths(rutaOptima[0][i+1], adjacency_list, min_distance, previous);
-        
-        //el 4 que se usa en min_distance[4] es el destino
-        cout << "from "<<rutaOptima[0][i+1]<<" to "<<rutaOptima[0][i]<<": " /*min_distance[rutaOptima[0][i]] << */;
-        
-         //aqui tambien el 4 es el destino
-         list<int> path = DijkstraGetShortestPathTo(rutaOptima[0][i], previous);
-         copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
-         cout << endl;
-         
-    }
-    cout<<endl;
     
-    
-    /*
-    for (int i=0; i<domiciliarios; ++i) {
-        DijkstraComputePaths(ruta[i][0], adjacency_list, min_distance, previous);
-        
-        //el 4 que se usa en min_distance[4] es el destino
-        cout << "Distance from "<<ruta[i][0]<<" to "<<ruta[i][1]<<": " << min_distance[ruta[i][1]] << endl;
-        
-        //aqui tambien el 4 es el destino
-        list<int> path = DijkstraGetShortestPathTo(ruta[i][1], previous);
-        cout << "Path : ";
-        copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
-        cout << endl << endl;
-    }
-     */
-    /*
-    vector<int> min_distance;
-    vector<int> previous;
-    
-    //el cero que se pasa en esta funcion es el origen
-    DijkstraComputePaths(0, adjacency_list, min_distance, previous);
-    
-    //el 4 que se usa en min_distance[4] es el destino
-    cout << "Distance from 0 to 4: " << min_distance[4] << endl;
-    
-    //aqui tambien el 4 es el destino
-    list<int> path = DijkstraGetShortestPathTo(4, previous);
-    cout << "Path : ";
-    copy(path.begin(), path.end(), ostream_iterator<int>(cout, " "));
-    cout << endl;
-    */
     return 0;
 }
 
